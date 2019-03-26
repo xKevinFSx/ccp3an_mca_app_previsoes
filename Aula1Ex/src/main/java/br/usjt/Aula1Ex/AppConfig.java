fee3f2345ad4a47455157a1909eeac1d62469bd6
Kevin <kevin.fs2013@gmail.com>
2019-03-26 18:21:03 -0300

    Aula 2

--- Aula1Ex/src/main/java/br/usjt/Aula1Ex/AppConfig.java
+++ Aula1Ex/src/main/java/br/usjt/Aula1Ex/AppConfig.java
@@ -9,7 +9,6 @@ public class AppConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login", "/",
-				"/fazerLogin");
+		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login", "/", "/fazerLogin");
 	}
 }
